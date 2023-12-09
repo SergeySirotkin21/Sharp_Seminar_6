58cc855a5f405ed946b46e6e4207b41ea72e0357
Sergey Sirotkin <niktoris0@gmail.com>
2023-12-09 13:36:29 +0300

    Добавил решение

---
+++ task_001/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задача 1: Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива
+// a b c => “abcdef”
+// d e f
+ 
+char[,] array = {{'a','b','c'},{'d','e','f'}};
+char[] arr1 = new char[array.GetLength(1)];
+char[] arr2 = new char[array.GetLength(1)];
+for (int i  =0; i < array.GetLength(0); i++ )
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    arr1[j] = array[0,j];
+    arr2[j] = array[1,j];
+    }
+}
+string str1 = new string(arr1);
+string str2 = new string(arr2);
+
+System.Console.WriteLine(string.Join("", str1, str2));
+
+
---
+++ task_003/Program.cs
@@ -0,0 +1,22 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+System.Console.Write("Введите строку для проверки на полиндром: ");
+string? str = Console.ReadLine();
+char[] char1 = new char[str!.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    char1[i] = str[str.Length -1 -i];
+}
+string str2 = new string(char1);
+System.Console.Write(str);
+if (str == str2)
+System.Console.WriteLine(" => Да");
+else
+System.Console.WriteLine(" => Нет");
+
